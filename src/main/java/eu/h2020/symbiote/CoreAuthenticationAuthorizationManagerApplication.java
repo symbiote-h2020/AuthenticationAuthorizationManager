1cf9a681ef079d5388a12de35b442a0901a9ca74
tipech <tipech@intracom-telecom.com>
2016-12-06 11:42:18 +0200

    Initial commit, skeleton microservice.

---
+++ src/main/java/eu/h2020/symbiote/CoreAuthenticationAuthorizationManagerApplication.java
@@ -0,0 +1,37 @@
+package eu.h2020.symbiote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+
+
+/**
+ * Created by mateuszl on 22.09.2016.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CoreAuthenticationAuthorizationManagerApplication {
+
+	private static Log log = LogFactory.getLog(CoreAuthenticationAuthorizationManagerApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreAuthenticationAuthorizationManagerApplication.class, args);
+
+        try {
+            // Subscribe to RabbitMQ messages
+        } catch (Exception e) {
+            log.error("Error occured during subscribing from Core Authentication & Authorization Manager", e);
+        }
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler() {
+        return new AlwaysSampler();
+    }
+
+}
