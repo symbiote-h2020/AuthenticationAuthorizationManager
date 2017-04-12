c0b78d3b2a9700b22b283bd3d7c1823398160e51
Mikolaj Dobski <mikolaj.dobski@man.poznan.pl>
2017-04-12 18:36:35 +0200

    Travis configuration update and remove of redundant class

--- merge_script.sh
+++ merge_script.sh
@@ -1,17 +1,9 @@
 #!/bin/bash -e
 
-
-: "${BRANCHES_TO_MERGE_REGEX?}" "${BRANCH_TO_MERGE?}"
-: "${GITHUB_SECRET_TOKEN?}" "${GITHUB_REPO?}" "${REPO_TEMP?}"
+: "${GITHUB_SECRET_TOKEN?}" "${GITHUB_REPO?}"
 
 export GIT_COMMITTER_EMAIL='travis@travis'
 export GIT_COMMITTER_NAME='Travis CI'
-
-if ! grep -q "$BRANCHES_TO_MERGE_REGEX" <<< "$TRAVIS_BRANCH"; then
-    printf "Current branch %s doesn't match regex %s, exiting\\n" \
-        "$TRAVIS_BRANCH" "$BRANCHES_TO_MERGE_REGEX" >&2
-    exit 0
-fi
 
 printf 'Removing staging branch as it has been handled already\n'
 printf 'git push %s :staging >/dev/null 2>&1\n' "$GITHUB_REPO"
--- pushing_script.sh
+++ pushing_script.sh
@@ -1,21 +1,8 @@
 #!/bin/bash -e
 
-
-: "${BRANCHES_TO_MERGE_REGEX?}" "${BRANCH_TO_MERGE_INTO?}"
-: "${GITHUB_SECRET_TOKEN?}" "${GITHUB_REPO?}" "${REPO_TEMP?}"
-
-export GIT_COMMITTER_EMAIL='travis@travis'
-export GIT_COMMITTER_NAME='Travis CI'
-
-if ! grep -q "$BRANCHES_TO_MERGE_REGEX" <<< "$TRAVIS_BRANCH"; then
-    printf "Current branch %s doesn't match regex %s, exiting\\n" \
-        "$TRAVIS_BRANCH" "$BRANCHES_TO_MERGE_REGEX" >&2
-    exit 0
-fi
+: "${GITHUB_SECRET_TOKEN?}" "${GITHUB_REPO?}" 
 
 ## shellcheck disable=SC2164
-#cd "$REPO_TEMP"
-#printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
 
 push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
 
