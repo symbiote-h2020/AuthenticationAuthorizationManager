be7f4c6ec26356d04d07a94e1b678a1cf8989099
vthglyk <glykantv@student.ethz.ch>
2017-06-15 10:54:27 +0300

    Swagger2markup not working

---
+++ src/test/java/eu/h2020/symbiote/security/SwaggerConfig.java
@@ -0,0 +1,67 @@
+package eu.h2020.symbiote.security;
+
+
+import com.google.common.base.Predicates;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static springfox.documentation.builders.PathSelectors.ant;
+
+@EnableSwagger2
+@Configuration
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(SwaggerConfig.class);
+
+    @Bean
+    public Docket restApi() {
+        LOG.info("restApi()");
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+//                .securitySchemes(asList(
+//                        new OAuth(
+//                                "petstore_auth",
+//                                asList(new AuthorizationScope("write_pets", "modify pets in your account"),
+//                                        new AuthorizationScope("read_pets", "read your pets")),
+//                                Arrays.<GrantType>asList(new ImplicitGrant(new LoginEndpoint("http://petstore.swagger.io/api/oauth/dialog"), "tokenName"))
+//                        ),
+//                        new ApiKey("api_key", "api_key", "header")
+//                ))
+                .produces(producesSet())
+                .select()
+                .paths(Predicates.and(ant("/**"), Predicates.not(ant("/error")), Predicates.not(ant("/management/**")), Predicates.not(ant("/management*"))))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Authentication and Authorization Manager")
+                .description("Authentication and Authorization Manager API Description")
+                .contact(new Contact("TestName", "http:/test-url.com", "test@test.de"))
+                .license("GNU Lesser General Public License v3.0")
+                .licenseUrl("https://github.com/symbiote-h2020/AuthenticationAuthorizationManager/blob/master/LICENSE.txt")
+                .version("0.2.0")
+                .build();
+    }
+
+    private Set<String> producesSet() {
+        Set<String> set = new HashSet<String>();
+        set.add("application/json");
+
+        return set;
+    }
+}
