e02c851036e5534bcf36993d2bba93895d9919b4
Dariusz Krajewski <towerdweller@protonmail.ch>
2017-07-13 15:01:18 +0200

    Modified Embedded AMQP Engine instance to react to illegal characters in user's home path

--- src/test/java/eu/h2020/symbiote/security/utils/EmbeddedAMQPEngine.java
+++ src/test/java/eu/h2020/symbiote/security/utils/EmbeddedAMQPEngine.java
@@ -21,42 +21,48 @@ import java.text.Normalizer;
  */
 @Component
 public class EmbeddedAMQPEngine {
-
-
     private EmbeddedRabbitMqConfig embeddedRabbitMqConfig;
     private EmbeddedRabbitMq embeddedRabbitMq;
     private String extractionPath;
+
     public EmbeddedAMQPEngine() {
+        if (SystemUtils.IS_OS_WINDOWS && pathContainsInvalidChar()) {
+            avoidInvalidChar();
+            embeddedRabbitMqConfig = new EmbeddedRabbitMqConfig.Builder()
+                    .version(PredefinedVersion.LATEST)
+                    .extractionFolder(new File(extractionPath + "/rabbitTemps"))
+                    .build();
+        } else {
+            embeddedRabbitMqConfig = new EmbeddedRabbitMqConfig.Builder()
+                    .version(PredefinedVersion.LATEST)
+                    .build();
+        }
 
-        avoidInvalidChar(); //  Set extraction path to a valid one
-
-        embeddedRabbitMqConfig = new EmbeddedRabbitMqConfig.Builder()
-                .version(PredefinedVersion.LATEST)
-                .extractionFolder(new File(extractionPath + "/rabbitTemps"))
-                .build();
         embeddedRabbitMq = new EmbeddedRabbitMq(embeddedRabbitMqConfig);
-
         embeddedRabbitMq.start();
     }
 
     /*
-     *Verify that User Home path doesn't contain diacritics or illegal characters
+     *  Issue valid extracion path
      */
     public void avoidInvalidChar() {
-        if (SystemUtils.IS_OS_WINDOWS) {
-            CharSequence userHome = System.getProperty("user.home");
-            if (!Normalizer.isNormalized(userHome, Normalizer.Form.NFD)) {
-                Path tempPath = Paths.get(System.getProperty("java.io.tmpdir") + "/rabbitTemps");
-                if (Files.exists(tempPath)) {
-                    //  Success - Path exists, select it for extraction
-                    extractionPath = System.getProperty("java.io.tmpdir");
-                } else {
-                    //  Fail - Path does not exist - needs creation
-                    throw new RuntimeIOException("User Home Path contains illegal characters. Please create " +
-                            "new directory in C:/Temp/rabbitTemps");
-                }
-            }
-        } else extractionPath = "/tmp";
+        Path tempPath = Paths.get(System.getProperty("java.io.tmpdir") + "/rabbitTemps");
+        if (Files.exists(tempPath)) {
+            //  Success - Path exists, select it for extraction
+            extractionPath = System.getProperty("java.io.tmpdir");
+        } else {
+            //  Fail - Path does not exist - needs creation
+            throw new RuntimeIOException("User Home Path contains illegal characters. Please create " +
+                    "new directory in C:/Temp/rabbitTemps");
+        }
+    }
+
+    /*
+     *  Verify that User Home path doesn't contain diacritics or illegal characters
+     */
+    public boolean pathContainsInvalidChar() {
+        CharSequence homePath = System.getProperty("user.home");
+        return Normalizer.isNormalized(homePath, Normalizer.Form.NFD);
     }
 
     @PreDestroy
