c316702b771c26899d8f0adad71cebc1b7a0b22f
Jakub Toczek <jakub.toczek@man.poznan.pl>
2017-10-26 13:07:53 +0200

    Merge remote-tracking branch 'origin/develop' into ADM

--- src/main/java/eu/h2020/symbiote/security/listeners/rest/controllers/ManageUsersController.java
+++ src/main/java/eu/h2020/symbiote/security/listeners/rest/controllers/ManageUsersController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 /**
  * Spring controller to handle HTTPS requests related to the RESTful web services associated to users' management.
@@ -72,7 +71,7 @@ public class ManageUsersController implements IManageUsers {
                 return new ResponseEntity<>(new UserDetails(), e.getStatusCode());
             else
                 return new ResponseEntity<>(new UserDetails(), e.getStatusCode());
-        } catch (TimeoutException | IOException e) {
+        } catch (IOException e) {
             return null;
         }
     }
