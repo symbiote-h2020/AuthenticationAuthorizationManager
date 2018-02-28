c5e7f292663dca82a18ea3eb5156ea96fef733fb
Jakub Toczek <jakub.toczek@man.poznan.pl>
2018-02-28 11:01:04 +0100

    Federation Consistency check added in the FederationManagement AMQP Listener

--- src/main/java/eu/h2020/symbiote/security/listeners/amqp/consumers/FederationManagementRequestConsumersService.java
+++ src/main/java/eu/h2020/symbiote/security/listeners/amqp/consumers/FederationManagementRequestConsumersService.java
@@ -2,6 +2,7 @@ package eu.h2020.symbiote.security.listeners.amqp.consumers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.h2020.symbiote.model.mim.Federation;
+import eu.h2020.symbiote.model.mim.FederationMember;
 import eu.h2020.symbiote.security.commons.exceptions.custom.InvalidArgumentsException;
 import eu.h2020.symbiote.security.repositories.FederationsRepository;
 import org.apache.commons.logging.Log;
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Component
 public class FederationManagementRequestConsumersService {
@@ -26,6 +28,14 @@ public class FederationManagementRequestConsumersService {
     private String adminUsername;
     @Value("${aam.deployment.owner.password}")
     private String adminPassword;
+
+    private static boolean isFederationConsistent(Federation federation) {
+        //check of federation consistency - size of members should be the same as size of set of members' platformIds
+        return federation.getMembers().size() == federation.getMembers().parallelStream()
+                .map(FederationMember::getPlatformId)
+                .collect(Collectors.toSet())
+                .size();
+    }
 
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue,
@@ -47,38 +57,11 @@ public class FederationManagementRequestConsumersService {
                     || federation.getMembers() == null
                     || federation.getId().isEmpty())
                 throw new InvalidArgumentsException();
-
+            if (!isFederationConsistent(federation)) {
+                throw new InvalidArgumentsException("Some of the Federation Members' platform Ids are duplicated");
+            }
             if (federationsRepository.exists(federation.getId())) {
                 throw new InvalidArgumentsException("Federation already exists.");
-            }
-            federationsRepository.save(federation);
-
-        } catch (InvalidArgumentsException | IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue,
-            exchange = @Exchange(
-                    value = "${rabbit.exchange.federation}",
-                    ignoreDeclarationExceptions = "true",
-                    durable = "false",
-                    internal = "${rabbit.exchange.aam.internal}",
-                    type = "topic"),
-            key = "${rabbit.routingKey.federation.changed}"))
-    public void federationUpdate(String message) {
-        log.debug("[x] Received Federation to update");
-        ObjectMapper om = new ObjectMapper();
-        Federation federation;
-        try {
-            federation = om.readValue(message, Federation.class);
-            if (federation.getId() == null
-                    || federation.getMembers() == null
-                    || federation.getId().isEmpty())
-                throw new InvalidArgumentsException();
-            if (!federationsRepository.exists(federation.getId())) {
-                throw new InvalidArgumentsException("Federation doesn't exist.");
             }
             federationsRepository.save(federation);
 
@@ -109,6 +92,38 @@ public class FederationManagementRequestConsumersService {
             federationsRepository.delete(federationId);
 
         } catch (InvalidArgumentsException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue,
+            exchange = @Exchange(
+                    value = "${rabbit.exchange.federation}",
+                    ignoreDeclarationExceptions = "true",
+                    durable = "false",
+                    internal = "${rabbit.exchange.aam.internal}",
+                    type = "topic"),
+            key = "${rabbit.routingKey.federation.changed}"))
+    public void federationUpdate(String message) {
+        log.debug("[x] Received Federation to update");
+        ObjectMapper om = new ObjectMapper();
+        Federation federation;
+        try {
+            federation = om.readValue(message, Federation.class);
+            if (federation.getId() == null
+                    || federation.getMembers() == null
+                    || federation.getId().isEmpty())
+                throw new InvalidArgumentsException();
+            if (!isFederationConsistent(federation)) {
+                throw new InvalidArgumentsException("Some of the Federation Members' platform Ids are duplicated");
+            }
+            if (!federationsRepository.exists(federation.getId())) {
+                throw new InvalidArgumentsException("Federation doesn't exist.");
+            }
+            federationsRepository.save(federation);
+
+        } catch (InvalidArgumentsException | IOException e) {
             log.error(e.getMessage());
         }
     }
