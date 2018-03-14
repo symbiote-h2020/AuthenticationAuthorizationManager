c740e493ecd7d472f0356317fa237bbc7ba31cd4
Jakub Toczek <jakub.toczek@man.poznan.pl>
2018-03-14 10:04:03 +0100

    GetUserDetails tests reorganization. Cleanup.

--- src/main/java/eu/h2020/symbiote/security/listeners/amqp/consumers/GetUserDetailsConsumer.java
+++ src/main/java/eu/h2020/symbiote/security/listeners/amqp/consumers/GetUserDetailsConsumer.java
@@ -94,7 +94,8 @@ public class GetUserDetailsConsumer {
                     //  Checking User's credentials
                     if (passwordEncoder.matches(userManagementRequest.getUserCredentials().getPassword(), foundUser.getPasswordEncrypted())) {
                         userDetails = new UserDetailsResponse(
-                                HttpStatus.OK, new UserDetails(new Credentials(foundUser.getUsername(), ""), foundUser.getRecoveryMail(),
+                                HttpStatus.OK,
+                                new UserDetails(new Credentials(foundUser.getUsername(), ""), foundUser.getRecoveryMail(),
                                 foundUser.getRole(), foundUser.getAttributes(), foundUser.getClientCertificates())
                         );
                     } else
--- src/test/java/eu/h2020/symbiote/security/functional/UsersManagementFunctionalTests.java
+++ src/test/java/eu/h2020/symbiote/security/functional/UsersManagementFunctionalTests.java
@@ -1,33 +1,26 @@
 package eu.h2020.symbiote.security.functional;
 
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.RpcClient;
 import eu.h2020.symbiote.security.AbstractAAMAMQPTestSuite;
-import eu.h2020.symbiote.security.commons.enums.IssuingAuthorityType;
 import eu.h2020.symbiote.security.commons.enums.ManagementStatus;
 import eu.h2020.symbiote.security.commons.enums.OperationType;
 import eu.h2020.symbiote.security.commons.enums.UserRole;
 import eu.h2020.symbiote.security.commons.exceptions.custom.AAMException;
-import eu.h2020.symbiote.security.communication.payloads.*;
+import eu.h2020.symbiote.security.communication.payloads.Credentials;
+import eu.h2020.symbiote.security.communication.payloads.ErrorResponseContainer;
+import eu.h2020.symbiote.security.communication.payloads.UserDetails;
+import eu.h2020.symbiote.security.communication.payloads.UserManagementRequest;
 import eu.h2020.symbiote.security.repositories.entities.User;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeoutException;
 
 import static org.junit.Assert.*;
 
@@ -35,52 +28,8 @@ import static org.junit.Assert.*;
 public class UsersManagementFunctionalTests extends
         AbstractAAMAMQPTestSuite {
 
-    private static Log log = LogFactory.getLog(OtherListenersFunctionalTests.class);
-    private final String federatedOAuthId = "federatedOAuthId";
-    @Value("${aam.environment.platformAAMSuffixAtInterWorkingInterface}")
-    protected String platformAAMSuffixAtInterWorkingInterface;
-    @Value("${symbIoTe.core.interface.url:https://localhost:8443}")
-    protected String coreInterfaceAddress;
-    @Value("${rabbit.queue.get.user.details}")
-    private String getUserDetailsQueue;
-    private UserManagementRequest appUserRegistrationRequest;
-    private UserManagementRequest appUserUpdateRequest;
-    private UserDetails appUserDetails;
     @Autowired
     private RabbitTemplate rabbitTemplate;
-
-    private Connection connection;
-
-    private RpcClient getUserDetailsClient;
-    @Override
-    @Before
-    public void setUp() throws
-            Exception {
-        super.setUp();
-        // hack: set deployment type back to CORE
-        ReflectionTestUtils.setField(usersManagementService, "deploymentType", IssuingAuthorityType.CORE);
-        // user registration useful
-        appUserDetails = new UserDetails(new Credentials(
-                username, password), recoveryMail, UserRole.USER, new HashMap<>(), new HashMap<>());
-        appUserRegistrationRequest = new UserManagementRequest(new Credentials(AAMOwnerUsername, AAMOwnerPassword),
-                new Credentials(username, password), appUserDetails, OperationType.CREATE);
-        appUserUpdateRequest = new UserManagementRequest(new Credentials(AAMOwnerUsername, AAMOwnerPassword),
-                new Credentials(username, password), appUserDetails, OperationType.UPDATE);
-        // verify that our app is not in repository
-        assertNull(userRepository.findOne(username));
-        getUserDetailsClient = new RpcClient(getConnection().createChannel(), "", getUserDetailsQueue, 5000);
-    }
-
-    public Connection getConnection() throws IOException, TimeoutException {
-        if (connection == null) {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost(rabbitTemplate.getConnectionFactory().getHost());
-            factory.setUsername(rabbitTemplate.getConnectionFactory().getUsername());
-            factory.setPassword("guest");
-            this.connection = factory.newConnection();
-        }
-        return this.connection;
-    }
 
     @Test
     public void userManagementOverAMQPSuccess() throws
@@ -169,7 +118,7 @@ public class UsersManagementFunctionalTests extends
     }
 
     @Test(expected = AAMException.class)
-    public void userManagementOverRESTFailProperPayloadReceived() throws
+    public void userManagementOverRESTFail() throws
             AAMException {
         assertFalse(userRepository.exists(username));
         //update not existing user to create error
@@ -185,126 +134,4 @@ public class UsersManagementFunctionalTests extends
                 OperationType.UPDATE);
         aamClient.manageUser(userManagementRequest);
     }
-
-    //TODO change getUserDetails
-    @Test
-    public void requestUserDetailsUsingRPCClientOverAMQPSuccess() throws
-            IOException,
-            TimeoutException {
-        //  Registering user in database
-        User User = createUser(username, password, recoveryMail, UserRole.USER);
-        userRepository.save(User);
-        assertTrue(userRepository.exists(username));
-
-        byte[] response = getUserDetailsClient.primitiveCall(mapper.writeValueAsBytes(new
-                UserManagementRequest(new
-                Credentials(AAMOwnerUsername, AAMOwnerPassword), new Credentials(username, password),
-                null, null
-        )));
-
-        UserDetailsResponse userDetails = mapper.readValue(response,
-                UserDetailsResponse.class);
-
-        log.info("Retrieved username is: " + userDetails.getUserDetails().getCredentials().getUsername());
-        assertEquals(username, userDetails.getUserDetails().getCredentials().getUsername());
-        assertEquals(HttpStatus.OK, userDetails.getHttpStatus());
-    }
-
-    @Test
-    public void requestUserDetailsOverAMQPSuccess() throws
-            IOException {
-        //  Registering user in database
-        User User = createUser(username, password, recoveryMail, UserRole.USER);
-        userRepository.save(User);
-        assertTrue(userRepository.exists(username));
-
-        byte[] response = rabbitTemplate.sendAndReceive(getUserDetailsQueue, new Message(mapper.writeValueAsBytes(new
-                UserManagementRequest(new
-                Credentials(AAMOwnerUsername, AAMOwnerPassword), new Credentials(username, password),
-                null, null
-        )), new MessageProperties())).getBody();
-        UserDetailsResponse userDetails = mapper.readValue(response,
-                UserDetailsResponse.class);
-
-        log.info("Retrieved username is: " + userDetails.getUserDetails().getCredentials().getUsername());
-        assertEquals(username, userDetails.getUserDetails().getCredentials().getUsername());
-        assertEquals(HttpStatus.OK, userDetails.getHttpStatus());
-    }
-
-
-    @Test
-    public void requestUserDetailsOverAMQPFailsForNotExistingUser() throws
-            IOException {
-        //  Registering user in database
-        User User = createUser(username, password, recoveryMail, UserRole.USER);
-        userRepository.save(User);
-        assertTrue(userRepository.exists(username));
-
-        byte[] response = rabbitTemplate.sendAndReceive(getUserDetailsQueue, new Message(mapper.writeValueAsBytes(new
-                UserManagementRequest(new
-                Credentials(AAMOwnerUsername, AAMOwnerPassword), new Credentials("NotExistingUser", "Password"),
-                null, null
-        )), new MessageProperties())).getBody();
-
-        UserDetailsResponse userDetails = mapper.readValue(response,
-                UserDetailsResponse.class);
-
-        assertEquals(HttpStatus.BAD_REQUEST, userDetails.getHttpStatus());
-    }
-
-    @Test
-    public void requestUserDetailsOverAMQPFailsForWrongPassword() throws
-            IOException {
-        //  Registering user in database
-        User User = createUser(username, password, recoveryMail, UserRole.USER);
-        userRepository.save(User);
-        assertTrue(userRepository.exists(username));
-
-        byte[] response = rabbitTemplate.sendAndReceive(getUserDetailsQueue, new Message(mapper.writeValueAsBytes(new
-                UserManagementRequest(new
-                Credentials(AAMOwnerUsername, AAMOwnerPassword), new Credentials(username, "wrongPassword"),
-                null, null
-        )), new MessageProperties())).getBody();
-
-        UserDetailsResponse userDetails = mapper.readValue(response,
-                UserDetailsResponse.class);
-
-        assertEquals(HttpStatus.UNAUTHORIZED, userDetails.getHttpStatus());
-    }
-
-    @Test
-    public void requestUserDetailsOverAMQPFailsForRequestWithoutUserCredentials() throws
-            IOException {
-        //  Registering user in database
-        User User = createUser(username, password, recoveryMail, UserRole.USER);
-        userRepository.save(User);
-        assertTrue(userRepository.exists(username));
-
-        byte[] response = rabbitTemplate.sendAndReceive(getUserDetailsQueue, new Message(mapper.writeValueAsString(new
-                UserManagementRequest(new
-                Credentials(AAMOwnerUsername, AAMOwnerPassword), null,
-                null, null
-        )).getBytes(), new MessageProperties())).getBody();
-
-        UserDetailsResponse userDetails = mapper.readValue(response,
-                UserDetailsResponse.class);
-        assertEquals(HttpStatus.UNAUTHORIZED, userDetails.getHttpStatus());
-    }
-
-    @Test
-    public void getUserDetailsFailsForIncorrectAdminPassword() throws
-            IOException {
-
-        byte[] response = rabbitTemplate.sendAndReceive(getUserDetailsQueue, new Message(mapper.writeValueAsString(new
-                UserManagementRequest(new
-                Credentials(AAMOwnerUsername, "wrongPassword"), new Credentials(username, password),
-                null, null
-        )).getBytes(), new MessageProperties())).getBody();
-
-        UserDetailsResponse userDetails = mapper.readValue(response,
-                UserDetailsResponse.class);
-        assertEquals(HttpStatus.FORBIDDEN, userDetails.getHttpStatus());
-    }
-
-
 }
